8717c3505f40343960eb829f1661a4df41dea3fa
Gabidev97 <gabrielmmori51@outlook.com>
2023-05-18 13:24:51 -0300

    retirando mock

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -3,7 +3,6 @@ import Header from "@/components/template/Header";
 import Page from "@/components/template/Page";
 import TitlePageUser from "@/components/template/TitlePageUser";
 import { IconForms } from "@tabler/icons-react";
-import usuarioFalso from "@/data/constants/usuarioFalso";
 import UserForm from "@/components/user/useForm";
 import { useContext } from "react";
 import AuthenticationContext from "@/contexts/UserAuthentication";
